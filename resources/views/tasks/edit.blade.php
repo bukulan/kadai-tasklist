ad9ce907a216cb107759a679587fcad3f2e7bd73
Masahiro Kunimoto <bukulan17@gmail.com>
2018-02-06 15:59:46 +0000

    add column

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -6,6 +6,9 @@
     
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
     
+        {!! Form::label('status', '状況:') !!}
+        {!! Form::text('status') !!}
+        
         {!! Form::label('content', 'タスク:') !!}
         {!! Form::text('content') !!}
         
